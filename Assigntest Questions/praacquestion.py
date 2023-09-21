5b84f2cd7a9c0b6eaff062dcb85782cd227abea1
hassan2112d <hassan2112d@aptechgdn.net>
2023-09-21 18:21:09 +0500

    hello

--- Assigntest Questions/praacquestion.py
+++ Assigntest Questions/praacquestion.py
@@ -2,7 +2,7 @@
 #adds the line of comments to it declared  by the 'comments' variable, and returns the size of the new file.
 #Fill in the gaps to create a script called "program.py".
 
-
+# import os
 # def create_python_script(filename):
 #     comments = "# Start of a new Python program"
 #     with open(filename, 'w') as file:
---
+++ week 2/CSV_FILES.py
@@ -0,0 +1,9 @@
+import csv
+
+f = open("second.html")
+
+hello = csv.reader(f)
+
+for row in hello:
+    name,age,surname= row
+    print("Name : {} , Age : {} , Surname : {} ".format(name,age,surname))
